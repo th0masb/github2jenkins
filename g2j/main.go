a2ee26416445b4ba16c9544e4cdf00acbfe815f5
Thomas Ball <tomoliverball@gmail.com>
2020-06-12 17:56:52 +0100

    [G2J-001] Implemented basic hello world web server

--- g2j/main.go
+++ g2j/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
     "fmt"
+    "log"
+    "net/http"
 )
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
 
 func main() {
     fmt.Printf("About to serve\n")
+    http.HandleFunc("/path", handler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
